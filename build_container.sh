14483c11e384fe812c5087ebfc32d99e1b567858
Yegor Rzhavsky <rzhavsky@evoplus.ca>
2017-11-14 15:18:21 +0300

    Change place

---
+++ build_container.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build --no-cache --file Dockerfile --tag url2pdf .
--- misc/deploy/build_container.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --no-cache --file Dockerfile --tag url2pdf ../../src
